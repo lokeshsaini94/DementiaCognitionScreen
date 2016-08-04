37026b0a2134e5d7e19b988107e90296c98f29d5
lokeshsaini94 <lokeshsaini94@gmail.com>
2016-08-05 05:09:09 +0530

    Update 1.2
    Removed all fragments used for different question and used
    view.visiblity to change layout for different questions.

--- app/src/main/java/hku/demscreen/hk/GlobalVariables.java
+++ app/src/main/java/hku/demscreen/hk/GlobalVariables.java
@@ -8,17 +8,6 @@ public class GlobalVariables extends Application {
     public static String userID = "";
     public static String userSex = "";
     public static boolean[] modulesSelected = {true, true, true, true, true, true, true, true, true, true};
+    public static int m1Question = 1;
     public static int[] m1Score = {0, 0, 0, 0};
-
-    public static void m1PreviousPage() {
-        Module01Activity.viewPager.setCurrentItem(getItem(-1), true);
-    }
-
-    public static void m1NextPage() {
-        Module01Activity.viewPager.setCurrentItem(getItem(+1), true);
-    }
-
-    private static int getItem(int i) {
-        return Module01Activity.viewPager.getCurrentItem() + i;
-    }
 }
--- app/src/main/java/hku/demscreen/hk/Module01Activity.java
+++ app/src/main/java/hku/demscreen/hk/Module01Activity.java
@@ -2,66 +2,185 @@ package hku.demscreen.hk;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import hku.demscreen.hk.Module01.M1Q1Fragment;
-import hku.demscreen.hk.Module01.M1Q2Fragment;
-import hku.demscreen.hk.Module01.M1Q3Fragment;
-import hku.demscreen.hk.Module01.M1Q4Fragment;
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 
 public class Module01Activity extends AppCompatActivity {
 
-    public static ViewPager viewPager;
-    PagerAdapter pagerAdapter;
+    String Tag = "Module01Activity";
+
+    ImageView sound;
+    ImageView scoreCorrect;
+    ImageView scoreWrong;
+    ImageView previousQuestion;
+    ImageView nextQuestion;
+    ImageView m1qImage;
+    TextView m1qNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_module01);
 
-        viewPager = (ViewPager) findViewById(R.id.view_pager01);
-        pagerAdapter = new PagerAdapter(getSupportFragmentManager());
-        viewPager.setAdapter(pagerAdapter);
+        sound = (ImageView) findViewById(R.id.sound);
+        scoreCorrect = (ImageView) findViewById(R.id.score_correct);
+        scoreWrong = (ImageView) findViewById(R.id.score_wrong);
+        m1qImage = (ImageView) findViewById(R.id.m1q_image);
+        m1qNumber = (TextView)findViewById(R.id.m1q_number);
+
+        sound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Sound will Play", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        scoreCorrect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (GlobalVariables.m1Question == 1) {
+                    GlobalVariables.m1Score[0] = 1;
+                    questionIncrement();
+                }else if (GlobalVariables.m1Question == 2) {
+                    GlobalVariables.m1Score[1] = 1;
+                    questionIncrement();
+                }else if (GlobalVariables.m1Question == 3) {
+                    GlobalVariables.m1Score[2] = 1;
+                    questionIncrement();
+                }else if (GlobalVariables.m1Question == 4) {
+                    GlobalVariables.m1Score[3] = 1;
+                } else {
+                    Log.d(Tag, "Error: Something went wrong (Module01Activity.scoreCorrect.setOnClickListener)");
+                }
+                Toast.makeText(getApplicationContext(), "Correct Answer", Toast.LENGTH_SHORT).show();
+                setView();
+            }
+        });
+
+        scoreWrong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (GlobalVariables.m1Question == 1) {
+                    GlobalVariables.m1Score[0] = 0;
+                    questionIncrement();
+                }else if (GlobalVariables.m1Question == 2) {
+                    GlobalVariables.m1Score[1] = 0;
+                    questionIncrement();
+                }else if (GlobalVariables.m1Question == 3) {
+                    GlobalVariables.m1Score[2] = 0;
+                    questionIncrement();
+                }else if (GlobalVariables.m1Question == 4) {
+                    GlobalVariables.m1Score[3] = 0;
+                } else {
+                    Log.d(Tag, "Error: Something went wrong (Module01Activity.scoreWrong.setOnClickListener)");
+                }
+                Toast.makeText(getApplicationContext(), "Wrong Answer", Toast.LENGTH_SHORT).show();
+                setView();
+            }
+        });
+
+        previousQuestion = (ImageView) findViewById(R.id.previous_question);
+        nextQuestion = (ImageView) findViewById(R.id.next_question);
+
+        previousQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (GlobalVariables.m1Question == 1) {
+                    Log.d(Tag, "Error: previous button tapped on m1q1 (Module01Activity)");
+                }else if (GlobalVariables.m1Question == 2) {
+                    questionDecrement();
+                }else if (GlobalVariables.m1Question == 3) {
+                    questionDecrement();
+                }else if (GlobalVariables.m1Question == 4) {
+                    questionDecrement();
+                } else {
+                    Log.d(Tag, "Error: Something went wrong (Module01Activity.previousQuestion.setOnClickListener)");
+                }
+                setView();
+            }
+        });
+
+        nextQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (GlobalVariables.m1Question == 1) {
+                    questionIncrement();
+                }else if (GlobalVariables.m1Question == 2) {
+                    questionIncrement();
+                }else if (GlobalVariables.m1Question == 3) {
+                    questionIncrement();
+                }else if (GlobalVariables.m1Question == 4) {
+                    Log.d(Tag, "Error: next button tapped on m1q4 (Module01Activity)");
+                } else {
+                    Log.d(Tag, "Error: Something went wrong (Module01Activity.nextQuestion.setOnClickListener)");
+                }
+                setView();
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_module_one);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Next Module will start.", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "q1= " + GlobalVariables.m1Score[0] + " q2= " + GlobalVariables.m1Score[1] + " q3= " + GlobalVariables.m1Score[2] + " q4= " + GlobalVariables.m1Score[3], Toast.LENGTH_SHORT).show();
             }
         });
     }
 
-    class PagerAdapter extends FragmentPagerAdapter {
-        public PagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
+    private void questionIncrement() {
+        GlobalVariables.m1Question = GlobalVariables.m1Question + 1;
+        animateActivityRight();
+    }
 
-        @Override
-        public Fragment getItem(int position) {
-            switch (position) {
-                case 0:
-                    return new M1Q1Fragment();
-                case 1:
-                    return new M1Q2Fragment();
-                case 2:
-                    return new M1Q3Fragment();
-                case 3:
-                    return new M1Q4Fragment();
-                default:
-                    return null;
-            }
-        }
+    private void questionDecrement() {
+        GlobalVariables.m1Question = GlobalVariables.m1Question - 1;
+        animateActivityLeft();
+    }
 
-        @Override
-        public int getCount() {
-            return 4;
+    private void setView() {
+        if (GlobalVariables.m1Question == 1) {
+            previousQuestion.setVisibility(View.GONE);
+            nextQuestion.setVisibility(View.VISIBLE);
+            m1qImage.setImageResource(R.drawable.m1q1);
+            m1qNumber.setText("1/4");
+        } else if (GlobalVariables.m1Question == 2) {
+            previousQuestion.setVisibility(View.VISIBLE);
+            nextQuestion.setVisibility(View.VISIBLE);
+            m1qImage.setImageResource(R.drawable.m1q2);
+            m1qNumber.setText("2/4");
+        } else if (GlobalVariables.m1Question == 3) {
+            previousQuestion.setVisibility(View.VISIBLE);
+            nextQuestion.setVisibility(View.VISIBLE);
+            m1qImage.setImageResource(R.drawable.m1q3);
+            m1qNumber.setText("3/4");
+        } else if (GlobalVariables.m1Question == 4) {
+            previousQuestion.setVisibility(View.VISIBLE);
+            nextQuestion.setVisibility(View.GONE);
+            m1qImage.setImageResource(R.drawable.m1q4);
+            m1qNumber.setText("4/4");
+        } else {
+            Toast.makeText(getApplicationContext(), "Something went wrong (Module01Activity.setView)", Toast.LENGTH_SHORT).show();
+            GlobalVariables.m1Question = 1;
+            setView();
         }
+    }
+
+    private void animateActivityLeft() {
+        YoYo.with(Techniques.SlideInLeft)
+                .duration(500)
+                .playOn(findViewById(R.id.content_m1q));
+    }
+
+    private void animateActivityRight() {
+        YoYo.with(Techniques.SlideInRight)
+                .duration(500)
+                .playOn(findViewById(R.id.content_m1q));
     }
 }
